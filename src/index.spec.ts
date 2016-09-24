d337491d3ec055caa570057f1203a9bd32a8e384
Denis Malinochkin <dmalinochkin@rambler.ru>
2016-09-24 19:57:02 +0300

    Update tests

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,37 +5,37 @@ import * as assert from 'assert';
 import { resolveOne, resolveMany } from './index';
 
 describe('Resolve one module path', () => {
-	it('Should work with local modules', () => {
+	it('Search `mocha` module path.', () => {
 		return resolveOne('mocha').then((filepath) => {
-			assert.equal(filepath, 'node_modules/mocha');
+			assert.ok(/node_modules\/mocha$/.test(filepath));
 		});
 	});
 
-	it('Should return a directory contains modules', () => {
-		return resolveOne('npm', '.', { resolveDir: true }).then((filepath) => {
-			assert.ok(/node_modules/.test(filepath));
-		});
-	});
-
-	it('Should work with global modules', () => {
+	it('Search `npm` module path.', () => {
 		return resolveOne('npm').then((filepath) => {
-			assert.ok(/node_modules\/npm/.test(filepath));
+			assert.ok(/node_modules\/npm$/.test(filepath));
 		});
 	});
 
-	it('Should work with modules with `resolveOnlyByPrefix` option', () => {
+	it('Search `npm` module path and return only dir.', () => {
+		return resolveOne('npm', '.', { resolveDir: true }).then((filepath) => {
+			assert.ok(/node_modules$/.test(filepath));
+		});
+	});
+
+	it('Search `npm` module with `resolveOnlyByPrefix` option.', () => {
 		return resolveOne('npm', '.', { resolveOnlyByPrefix: true }).then((filepath) => {
-			assert.ok(/node_modules\/npm/.test(filepath));
+			assert.ok(/node_modules\/npm$/.test(filepath));
 		});
 	});
 
-	it('Should give an error if module is not found', () => {
+	it('Should give an error if module is not found.', () => {
 		return resolveOne('notFound').then((filepath) => {
 			assert.equal(filepath, undefined);
 		});
 	});
 
-	it('Should work with the cache path', () => {
+	it('Should return cached path.', () => {
 		return resolveOne('npm', '.', { cache: 'cache/path/to/module' }).then((filepath) => {
 			assert.equal(filepath, 'cache/path/to/module');
 		});
@@ -43,41 +43,41 @@ describe('Resolve one module path', () => {
 });
 
 describe('Resolve many modules paths', () => {
-	it('Should work with local modules', () => {
+	it('Search `mocha` & `tslint` module path.', () => {
 		return resolveMany(['mocha', 'tslint']).then((filepaths) => {
-			assert.equal(filepaths[0], 'node_modules/mocha');
-			assert.equal(filepaths[1], 'node_modules/tslint');
+			assert.ok(/node_modules\/mocha$/.test(filepaths[0]));
+			assert.ok(/node_modules\/tslint$/.test(filepaths[1]));
 		});
 	});
 
-	it('Should return a directory contains modules', () => {
-		return resolveMany(['mocha', 'npm'], '.', { resolveDir: true }).then((filepaths) => {
-			assert.ok(/node_modules/.test(filepaths[0]));
-			assert.ok(/node_modules/.test(filepaths[1]));
-		});
-	});
-
-	it('Should work with global modules', () => {
+	it('Search `npm` module path.', () => {
 		return resolveMany(['npm']).then((filepaths) => {
-			assert.ok(/node_modules\/npm/.test(filepaths[0]));
+			assert.ok(/node_modules\/npm$/.test(filepaths[0]));
 		});
 	});
 
-	it('Should work with modules with `resolveOnlyByPrefix` option', () => {
+	it('Search `mocha` & `npm` module path and return only dir.', () => {
+		return resolveMany(['mocha', 'npm'], '.', { resolveDir: true }).then((filepaths) => {
+			assert.ok(/node_modules$/.test(filepaths[0]));
+			assert.ok(/node_modules$/.test(filepaths[1]));
+		});
+	});
+
+	it('Search `npm` module with `resolveOnlyByPrefix` option.', () => {
 		return resolveMany(['npm'], '.', { resolveOnlyByPrefix: true }).then((filepaths) => {
-			assert.ok(/node_modules\/npm/.test(filepaths[0]));
+			assert.ok(/node_modules\/npm$/.test(filepaths[0]));
 		});
 	});
 
-	it('Should give an error if module is not found', () => {
+	it('Should give an error if module is not found.', () => {
 		return resolveMany(['mocha', 'notFound', 'npm']).then((filepaths) => {
-			assert.ok(/node_modules\/mocha/.test(filepaths[0]));
+			assert.ok(/node_modules\/mocha$/.test(filepaths[0]));
 			assert.equal(filepaths[1], undefined);
-			assert.ok(/node_modules\/npm/.test(filepaths[2]));
+			assert.ok(/node_modules\/npm$/.test(filepaths[2]));
 		});
 	});
 
-	it('Should work with the cache path', () => {
+	it('Should return cached path.', () => {
 		return resolveMany(['npm'], '.', { cache: 'cache/path/to/module' }).then((filepaths) => {
 			assert.equal(filepaths[0], 'cache/path/to/module');
 		});
